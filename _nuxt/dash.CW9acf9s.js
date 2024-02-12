f03ca0528ee57897d26aa919aa5fcbb7110b08d0
m.r <mr@example.com>
2024-02-11 18:25:01 -0800

    init

---
+++ _nuxt/dash.CW9acf9s.js
@@ -0,0 +1,1 @@
+import{L as e,v as o,t}from"./entry.CAg9ALth.js";const c={__name:"dash",setup(n){return e(()=>{console.log("not mounted")}),(a,r)=>(t(),o("p",null,"kl"))}};export{c as default};
---
+++ _nuxt/index.BYx9Ha1k.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,t as c}from"./entry.CAg9ALth.js";const n={};function r(o,s){return c(),t("p",null,"test")}const a=e(n,[["render",r]]);export{a as default};
